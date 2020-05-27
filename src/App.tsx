d519a1aa0825ca3c515324dac40cc2cbe688d3cf
Paul Shen <paulshen0@gmail.com>
2020-05-27 15:48:17 -0700

    createSourceFile

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,87 @@
 import React from "react";
 import { css } from "@emotion/core";
 import MonacoEditor from "react-monaco-editor";
+import * as ts from "typescript";
+
+function Editor({
+  code,
+  onChange,
+}: {
+  code: string;
+  onChange: (code: string) => void;
+}) {
+  return (
+    <MonacoEditor
+      language="typescript"
+      theme="vs-dark"
+      value={code}
+      options={{}}
+      onChange={(newValue) => {
+        onChange(newValue);
+      }}
+      editorWillMount={(monaco) => {
+        monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+          target: monaco.languages.typescript.ScriptTarget.ESNext,
+          allowNonTsExtensions: true,
+        });
+      }}
+      editorDidMount={(editor) => {
+        editor.focus();
+      }}
+    />
+  );
+}
+
+const SourceFile = React.memo(
+  ({ sourceFile }: { sourceFile: ts.SourceFile }) => {
+    return (
+      <div>
+        <div>{sourceFile.fileName}</div>
+      </div>
+    );
+  }
+);
+
+function Output({ code }: { code: string }) {
+  const sourceFile = React.useMemo(
+    () => ts.createSourceFile("index.ts", code, ts.ScriptTarget.Latest),
+    [code]
+  );
+  return (
+    <div
+      css={css`
+        font-family: SF Mono;
+        white-space: pre;
+      `}
+    >
+      <SourceFile sourceFile={sourceFile} />
+    </div>
+  );
+}
 
 function App() {
   const [code, setCode] = React.useState("");
   return (
     <div
       css={css`
-        width: 50vw;
-        height: 100vh;
+        display: flex;
       `}
     >
-      <MonacoEditor
-        language="typescript"
-        theme="vs-dark"
-        value={code}
-        options={{}}
-        onChange={(newValue) => {
-          setCode(newValue);
-        }}
-        editorWillMount={(monaco) => {
-          monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-            target: monaco.languages.typescript.ScriptTarget.ESNext,
-            allowNonTsExtensions: true,
-          });
-        }}
-        editorDidMount={(editor) => {
-          editor.focus();
-        }}
-      />
+      <div
+        css={css`
+          width: 50vw;
+          height: 100vh;
+        `}
+      >
+        <Editor code={code} onChange={setCode} />
+      </div>
+      <div
+        css={css`
+          width: 50vw;
+        `}
+      >
+        <Output code={code} />
+      </div>
     </div>
   );
 }
