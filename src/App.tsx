c1fefd330a63dbbf8e296fdd0f4d2bbaa5177bdd
Paul Shen <paulshen0@gmail.com>
2020-05-27 14:00:49 -0700

    monaco-editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import MonacoEditor from "react-monaco-editor";
 
 function App() {
-  return <div>Hello</div>;
+  const [code, setCode] = React.useState("");
+  return (
+    <div style={{ height: "100vh", width: "80vw" }}>
+      <MonacoEditor
+        language="typescript"
+        theme="vs-dark"
+        value={code}
+        options={{}}
+        onChange={(newValue) => {
+          setCode(newValue);
+        }}
+        editorWillMount={(monaco) => {
+          monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+            target: monaco.languages.typescript.ScriptTarget.ESNext,
+            allowNonTsExtensions: true,
+          });
+        }}
+        editorDidMount={(editor) => {
+          editor.focus();
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
